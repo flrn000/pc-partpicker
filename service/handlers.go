ebe12872eb60d8bc21507c7107a9c75999965181
Iacob Florin <iacob.florin000@gmail.com>
2024-09-22 20:01:24 +0300

    add user register functionality

--- service/handlers.go
+++ service/handlers.go
@@ -1,8 +1,16 @@
 package service
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
 
-func handleLogin() http.Handler {
+	"github.com/flrn000/pc-partpicker/data"
+	"github.com/flrn000/pc-partpicker/types"
+	"github.com/flrn000/pc-partpicker/utils"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func handleLogin(userStore *data.UserStore) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 
@@ -10,10 +18,51 @@ func handleLogin() http.Handler {
 	)
 }
 
-func handleRegister() http.Handler {
+func handleRegister(userStore *data.UserStore) http.Handler {
+	type RegisterUserPayload struct {
+		UserName string `json:"user_name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
+			val, err := utils.Decode[RegisterUserPayload](r)
+			if err != nil {
+				utils.WriteError(w, r, http.StatusInternalServerError, err)
+				return
+			}
 
+			if _, err := userStore.GetByEmail(val.Email); err == nil {
+				utils.WriteError(w, r, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", val.Email))
+				return
+			}
+
+			hashedPassword, err := bcrypt.GenerateFromPassword([]byte(val.Password), bcrypt.DefaultCost)
+			if err != nil {
+				utils.WriteError(w, r, http.StatusInternalServerError, err)
+				return
+			}
+
+			user := &types.User{
+				UserName: val.UserName,
+				Email:    val.Email,
+				Password: string(hashedPassword),
+			}
+
+			err = userStore.Create(user)
+			if err != nil {
+				utils.WriteError(w, r, http.StatusInternalServerError, err)
+				return
+			}
+
+			w.Header().Set("Location", fmt.Sprintf("api/v1/users/%d", user.ID))
+
+			err = utils.Encode(w, r, http.StatusCreated, user)
+			if err != nil {
+				utils.WriteError(w, r, http.StatusInternalServerError, err)
+				return
+			}
 		},
 	)
 }
