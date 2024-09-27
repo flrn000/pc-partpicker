e877b2a2c4b6aa7b9acb0ff7e3d502de055da46f
Iacob Florin <iacob.florin000@gmail.com>
2024-09-27 13:58:43 +0300

    basic form field validator

--- utils/utils.go
+++ utils/utils.go
@@ -3,8 +3,52 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"strings"
+	"unicode/utf8"
 )
+
+type Validator struct {
+	FieldErrors map[string]string
+}
+
+func (v *Validator) IsValid() bool {
+	return len(v.FieldErrors) == 0
+}
+
+func (v *Validator) AddFieldError(key, message string) {
+	if v.FieldErrors == nil {
+		v.FieldErrors = make(map[string]string)
+	}
+
+	if _, exists := v.FieldErrors[key]; !exists {
+		v.FieldErrors[key] = message
+	}
+}
+
+func (v *Validator) CheckField(ok bool, key, message string) {
+	if !ok {
+		v.AddFieldError(key, message)
+	}
+}
+
+func IsNotBlank(val string) bool {
+	return strings.TrimSpace(val) != ""
+}
+
+func MaxChars(val string, n int) bool {
+	return utf8.RuneCountInString(val) <= n
+}
+
+func IsPermittedInt(val int, permittedValues ...int) bool {
+	for _, v := range permittedValues {
+		if val == v {
+			return true
+		}
+	}
+	return false
+}
 
 func Encode[T any](w http.ResponseWriter, r *http.Request, status int, v T) error {
 	w.Header().Set("Content-Type", "application/json")
@@ -24,5 +68,6 @@ func Decode[T any](r *http.Request) (T, error) {
 }
 
 func WriteError(w http.ResponseWriter, r *http.Request, status int, err error) {
+	slog.Error(err.Error())
 	Encode(w, r, status, map[string]string{"error": err.Error()})
 }
